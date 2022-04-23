163215c6696a9656a8b4f99853449c97c0c623a1
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2022-04-22 19:41:48 -0500

    Add files via upload

--- src/map/Tile.java
+++ src/map/Tile.java
@@ -1,1 +1,61 @@
+package map;
 
+import entity.Entity;
+
+/**
+ * Tile class represents a single Tile
+ * @author Vachia Thoj
+ *
+ */
+public class Tile extends Entity 
+{
+	//integer value of Tile
+	private int value;
+	
+	//Flag to see if tile is blocked; blocked meaning tile cannot be "stepped" on or collided with
+	private boolean blocked;
+	
+	//min and max possible values for Tile
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 1;
+	private static final int DEFAULT_VALUE = MIN_VALUE;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param x integer x coordinate of Tile
+	 * @param y integer y coordinate of Tile
+	 * @param width integer width (pixels) of Tile
+	 * @param height integer height (pixels) of Tile
+	 * @param value integer value associated with Tile
+	 */
+	public Tile(int x, int y, int width, int height, int value)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		setValue(value);
+		
+		this.blocked = false;
+	}
+	
+	//Getter Methods
+	public int getValue() {return value;}
+	public boolean isBlocked() {return blocked;}
+	
+	
+	//Setter Methods
+	public void setValue(int value) 
+	{
+		if(value < MIN_VALUE || value > MAX_VALUE)
+		{
+			this.value = DEFAULT_VALUE;
+		}
+		else
+		{
+			this.value = value;
+		}
+	}
+	public void setBlocked(boolean b) {blocked = b;}
+}
---
+++ src/map/TileMap.java
@@ -0,0 +1,170 @@
+package map;
+
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.awt.Color;
+import java.awt.BasicStroke;
+
+/**
+ * TileMap class represents a map (Tile Map)
+ * @author Vachia Thoj
+ *
+ */
+public class TileMap 
+{
+	//x and y coordinate of where tileMap starts
+	private int startX;
+	private int startY;
+	
+	//x and y coordinate of where tileMap ends
+	private int endX;
+	private int endY;
+	
+	//Total width and height of tileMap
+	private int width;
+	private int height;
+	
+	//Size of a tile in tileMap
+	private int tileSize;
+	
+	//Number columns and rows of tiles
+	private int numCols;
+	private int numRows;
+	
+	//The tileMap
+	private Tile[][] tileMap;
+	
+	//Flag to outline map
+	private boolean outlined;
+	
+	/**
+	 * Constructor
+	 * @param address String address of TileMap file
+	 */
+	public TileMap(String address)
+	{
+		this.startX = 0;
+		this.startY = 0;
+		this.tileSize = 32;
+		
+		loadMap(address);
+		
+		this.width = numCols * tileSize;
+		this.height = numRows * tileSize;
+		
+		this.endY = startX + width;
+		this.endX = startY + height;
+		
+		this.outlined = true;
+	}
+	
+	/**
+	 * Method that attempts to load a tile map from a given tile map file
+	 * @param address file path of tile map data 
+	 */
+	private void loadMap(String address)
+	{
+		try
+		{
+			InputStream inputStream = getClass().getResourceAsStream(address);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			
+			numCols = Integer.parseInt(bufferedReader.readLine());
+			numRows = Integer.parseInt(bufferedReader.readLine());
+			
+			tileMap = new Tile[numRows][numCols];
+			
+			String delimeter = "\\s+";
+			
+			for(int i = 0; i < numRows; i++)
+			{
+				String line = bufferedReader.readLine();
+				String [] tokens = line.split(delimeter);
+				
+				for(int j = 0; j < numCols; j++)
+				{
+					int x = j * tileSize;
+					int y = i * tileSize;
+					int value = Integer.parseInt(tokens[j]);
+					tileMap[i][j] = new Tile(x, y, tileSize, tileSize, value);
+					
+					if(value == 1)
+					{
+						tileMap[i][j].setBlocked(true);
+					}
+				}
+			}
+			
+			inputStream.close();
+			bufferedReader.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error loading Map");
+			e.printStackTrace();
+		}
+	}
+	
+	//Getter Methods
+	public int getStartX() {return startX;}
+	public int getStartY() {return startY;}
+	public int getEndX() {return endX;}
+	public int getEndY() {return endY;}
+	public int getWidth() {return width;}
+	public int getHeight() {return height;}
+	public int getTileSize() {return tileSize;}
+	public int getNumCols() {return numCols;}
+	public int getNumRows() {return numRows;}
+	public Tile[][] getTileMap() {return tileMap;}
+	public boolean isOutlined() {return outlined;}
+	
+	public void setOutlined(boolean b) {outlined = b;}
+	
+	public Tile getTile(int col, int row) 
+	{	
+		if(row < 0 || row > (numRows - 1) ||
+		   col < 0 || col > (numCols - 1))
+		{
+			return null;
+		}
+		
+		return tileMap[row][col];
+	}
+	
+	
+	/**
+	 * Method that draws the TileMap
+	 * @param g The Graphics2D object to be drawn on
+	 */
+	public void draw(Graphics2D g)
+	{
+		//Draw tileMap
+		for(int i = 0; i < numRows; i++)
+		{
+			for(int j = 0; j < numCols; j++)
+			{
+				Tile temp = tileMap[i][j];
+				g.setStroke(new BasicStroke(1));
+				
+				if(temp.getValue() == 0)
+				{
+					g.setColor(Color.WHITE);
+				}
+				else
+				{
+					g.setColor(Color.BLACK);
+				}
+				
+				g.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+				
+				if(outlined == true)
+				{
+					g.setColor(Color.BLACK);
+					g.drawRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+				}
+			}
+		}
+	}
+}
