fa3d31240450b2fc74831fa82b08f1dfcd67e55d
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2022-04-22 19:25:46 -0500

    Add files via upload

--- src/manager/ImageManager.java
+++ src/manager/ImageManager.java
@@ -1,1 +1,103 @@
+package manager;
 
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+/**
+ * ImageManager class attempts to load image files and keeps track 
+ * of them
+ * 
+ * @author Vachia Thoj
+ *
+ */
+public class ImageManager 
+{
+	//For singleton
+	private static ImageManager imageManager;
+	
+	//To store Button images
+	private BufferedImage[] buttonImages;
+	
+	/**
+	 * Constructor (private)
+	 */
+	private ImageManager()
+	{
+		BufferedImage buttonSheet = loadImage("/images/ButtonSheet.png");
+		
+		loadButtonImages(buttonSheet);
+		
+		buttonSheet = null;
+	}
+	
+	/**
+	 * Method to be called to obtain ImageManager object (Singleton)
+	 * @return ImageManager object 
+	 */
+	public static ImageManager instance()
+	{
+		if(imageManager == null)
+		{
+			imageManager = new ImageManager();
+		}
+		
+		return imageManager;
+	}
+	
+	/**
+	 * Method that attempts to open/obtain an image
+	 * 
+	 * @param address String of the address location of the image
+	 * @return BufferedImage from the address provided, will return null if image cannot be found opened
+	 */
+	private BufferedImage loadImage(String address)
+	{
+		BufferedImage imageSheet = null;
+		
+		//Try to obtain an image
+		try {
+			//Obtain image from address
+			imageSheet = ImageIO.read(getClass().getResourceAsStream(address));
+			
+			return imageSheet;
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Error loading image");
+			System.exit(0);
+		}
+		return null;
+	}
+	
+	/**
+	 * Method that obtains individual button images from a Button sheet image
+	 * @param sheet A bufferedImage of Buttons
+	 */
+	private void loadButtonImages(BufferedImage sheet)
+	{
+		buttonImages = new BufferedImage[12];
+		int index = 0;
+		
+		for(int i = 0; i < 6; i++)
+		{
+			buttonImages[index] = sheet.getSubimage(i * 96, 0, 96, 32);
+			++index;
+		}
+		
+		for(int i = 0; i < 2; i++)
+		{
+			buttonImages[index] = sheet.getSubimage(i * 96, 32, 96, 32);
+			++index;
+		}
+		
+		for(int i = 0; i < 4; i++)
+		{
+			buttonImages[index] = sheet.getSubimage(((i * 32) + 192), 32, 32, 32);
+			++index;
+		}
+	}
+	
+	//Getter methods
+	public BufferedImage[] getButtonImages() {return buttonImages;}
+}
---
+++ src/manager/MouseManager.java
@@ -0,0 +1,71 @@
+package manager;
+
+/**
+ * MouseManager class keeps track and manages the mouses actions
+ * 
+ * @author Vachia Thoj
+ *
+ */
+public class MouseManager 
+{
+	//For singleton
+	private static MouseManager mouseManager;
+	
+	//Current location of mouse
+	private Point currentPoint;
+	
+	//Location where the mouse pressed
+	private Point pressedPoint;
+	
+	//Location where the mouse released
+	private Point releasedPoint;
+	
+	//Flag to see if mouse has bee pressed/released
+	private boolean mousePressed;
+	private boolean mouseReleased;
+	
+	/**
+	 * Constructor (private)
+	 */
+	private MouseManager()
+	{
+		currentPoint = null;
+		pressedPoint = null;
+		releasedPoint = null;
+		
+		mousePressed = false;
+		mouseReleased = false;
+	}
+	
+	/**
+	 * Method to be called in order to obtain MouseManager object (Singleton)
+	 * @return MouseManager object
+	 */
+	public static MouseManager instance()
+	{
+		if(mouseManager == null)
+		{
+			mouseManager = new MouseManager();
+		}
+		
+		return mouseManager;
+	}
+	
+	//Getter methods
+	public Point getPressedPoint() {return pressedPoint;}
+	public Point getReleasedPoint() {return releasedPoint;}
+	public Point getCurrentPoint() {return currentPoint;}
+	public boolean isMousePressed() {return mousePressed;}
+	public boolean isMouseReleased() {return mouseReleased;}
+		
+	//Setter methods
+	public void setPressedPoint(int mouseX, int mouseY) {pressedPoint = new Point(mouseX, mouseY);}
+	public void setReleasedPoint(int mouseX, int mouseY) {releasedPoint = new Point(mouseX, mouseY);}
+	public void setCurrentPoint(int mouseX, int mouseY) {currentPoint = new Point(mouseX, mouseY);}
+	public void setMousePressed(boolean b) {mousePressed = b;}
+	public void setMouseReleased(boolean b) {mouseReleased = b;}
+		
+	public void clearPressedPoint() {pressedPoint = null;}
+	public void clearReleasedPoint() {releasedPoint = null;}
+	public void clearCurrentPoint() {currentPoint = null;}
+}
