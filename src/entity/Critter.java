6945a4c575cb751317347bdc1741456764683e6e
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2022-04-22 19:37:33 -0500

    Add files via upload

---
+++ src/entity/Critter.java
@@ -0,0 +1,344 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.BasicStroke;
+import java.util.ArrayList;
+
+import main.GamePanel;
+import map.TileMap;
+import graph.*;
+
+/**
+ * Critter class represents a Critter
+ * @author Vachia Thoj
+ *
+ */
+public class Critter extends Entity 
+{
+	//Speed of Critter
+	private int speed;
+	
+	//Color of Critter
+	private Color color;
+	
+	//Direction Critter is moving
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+		
+	//Flag to see if player is moving
+	private boolean moving;
+	
+	private TileMap tileMap;
+	private Graph graph;
+	private ArrayList<Node> route;
+	private int currentRoute;
+	
+	//Target x and y coordinate Critter should move to
+	private int targetX;
+	private int targetY;
+	
+	//Timer and delay for Critter to move
+	private long moveTimer;
+	private long moveDelay;
+	
+	//Timer and delay to "highlight" Critter when it has eaten food
+	private boolean highlight;
+	private long highlightTimer;
+	private long highlightDelay;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param x integer x coordinate of Critter
+	 * @param y integer y coordinate of Critter
+	 * @param width integer width (pixels) of Critter
+	 * @param height integer height (pixels) of Critter
+	 * @param tileMap a TileMap
+	 */
+	public Critter(int x, int y, int width, int height, TileMap tileMap)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.tileMap = tileMap;
+		
+		this.speed = 4;
+		this.color = Color.RED;
+		
+		this.left = false;
+		this.right = false;
+		this.up = false;
+		this.down = false;
+		this.moving = false;
+		
+		this.graph = new Graph(tileMap);
+		this.route = null;
+		this.currentRoute = 0;
+		
+		this.moveTimer = System.nanoTime();
+		this.moveDelay = 10;
+		
+		this.highlight = false;
+		this.highlightDelay = 250;
+	}
+	
+	//Getter methods
+	public int getSpeed() {return speed;}
+	public Color getColor() {return color;}
+	public boolean isMoving() {return moving;}
+	public ArrayList<Node> getRoute() {return route;}
+	
+	//Setter methods
+	public void setSpeed(int speed) {this.speed = speed;}
+	public void setColor(Color color) {this.color = color;}
+	
+	public void highlightCritter() 
+	{
+		highlight = true;
+		highlightTimer = System.nanoTime();
+	}
+	
+	/**
+	 * Method that determines the next direction for Critter to move
+	 */
+	private void findDirection()
+	{
+		//do not find direction if Critter is still moving or if there is no route
+		if(moving == true || route == null)
+		{
+			return;
+		}
+		
+		//determine the next x and y coordinate for Critter to move to 
+		targetX = (route.get(currentRoute).getId() % tileMap.getNumCols()) * tileMap.getTileSize();
+		targetY = (route.get(currentRoute).getId() / tileMap.getNumCols()) * tileMap.getTileSize();
+		
+		//determine which direction to move to
+		if(targetX < x) //move left
+		{
+			left = true;
+			moving = true;
+		}
+		else if(targetX > x) //move right
+		{
+			right = true;
+			moving = true;
+		}
+		else if(targetY < y) //move up
+		{
+			up = true;
+			moving = true;
+		}
+		else if(targetY > y) //move down
+		{
+			down = true;
+			moving = true;
+		}
+	}
+	
+	/**
+	 * Method that increments the currentRoute index
+	 */
+	private void nextLocation()
+	{
+		if(route == null)
+		{
+			return;
+		}
+		
+		if(targetX == x && targetY == y)
+		{
+			++currentRoute;
+		}
+		
+		if(currentRoute >= route.size())
+		{
+			currentRoute = 0;
+			route = null;
+		}
+	}
+	
+	/**
+	 * Method that moves Critter
+	 */
+	private void move()
+	{
+		if(moving == true)
+		{
+			if(((System.nanoTime() - moveTimer) / 1000000) < moveDelay)
+			{
+				return;
+			}
+			else
+			{
+				moveTimer = System.nanoTime();
+			}
+		}
+		
+		if(left == true) //moving left
+		{
+			
+			if((x - speed) < targetX)
+			{
+				x = targetX;
+			}
+			else
+			{
+				x = x - speed;
+			}
+			
+			if(targetX == x)
+			{
+				left = false;
+				moving = false;
+			}
+		}
+		else if(right == true) //moving right
+		{
+			if((x + speed) > targetX)
+			{
+				x = targetX;
+			}
+			else
+			{
+				x = x + speed;
+			}
+			
+			if(targetX == x)
+			{
+				right = false;
+				moving = false;
+			}
+		}
+		else if(up == true) //moving up
+		{
+			if((y - speed) < targetY)
+			{
+				y = targetY;
+			}
+			else
+			{
+				y = y - speed;
+			}
+			
+			if(targetY == y)
+			{
+				up = false;
+				moving = false;
+			}
+		}
+		else if(down == true) //moving down
+		{
+			if(y + speed > targetY)
+			{
+				y = targetY;
+			}
+			else
+			{
+				y = y + speed;
+			}
+			
+			if(targetY == y)
+			{
+				down = false;
+				moving = false;
+			}
+		}
+	}
+	
+	/**
+	 * Method that checks if Player is at the edges of window
+	 * Won't allow Player to go beyond boundaries
+	 */
+	private void checkBoundary()
+	{
+		if(x < 0) //Left boundary
+		{
+			x = 0;
+		}
+		else if(x > GamePanel.WIDTH - width) //Right boundary
+		{
+			x = GamePanel.WIDTH - width;
+		}
+		
+		if(y < 0) //Top boundary
+		{
+			y = 0;
+		}
+		else if(y > GamePanel.HEIGHT - height) //Bottom boundary
+		{
+			y = GamePanel.HEIGHT - height;
+		}
+	}
+	
+	/**
+	 * Method that finds a path for Critter to travel
+	 * @param startLocation nodeList start index
+	 * @param endLocation nodeList end index
+	 */
+	public void findPath(int startLocation, int endLocation)
+	{	
+		Node[] nodeList = graph.getNodeList();
+		
+		for(int i = 0; i < nodeList.length; i++)
+		{
+			nodeList[i].setPrevious(null);
+			nodeList[i].setVisited(false);
+		}
+		
+		ShortestPath shortestPath = new ShortestPath(nodeList, nodeList[startLocation], nodeList[endLocation]);
+		//Use breadth first search to find shortest path
+		shortestPath.bfs();
+		
+		route = shortestPath.getRoute();
+	}
+	
+	/**
+	 * Method that updates the Critter
+	 */
+	public void update()
+	{
+		//Determine which direction to move 
+		findDirection();
+		
+		//Move Critter
+		move();
+		
+		//Obtain next location to move to
+		nextLocation();
+		
+		//Check if Critter has touched edges of window
+		checkBoundary();
+		
+		if(highlight == true)
+		{
+			//Stop highlighting Critter if time is up
+			if(((System.nanoTime() - highlightTimer) / 1000000) > highlightDelay)
+			{
+				highlight = false;
+			}
+		}
+	}
+	
+	/**
+	 * Method that draws the Critter
+	 * @param g The Graphics2D object to be drawn on
+	 */
+	public void draw(Graphics2D g)
+	{
+		//Draw Critter
+		g.setColor(color);
+		g.fillRect(x, y, width, height);
+		
+		if(highlight == true)
+		{ 
+			g.setColor(new Color(126, 10, 16));
+			g.setStroke(new BasicStroke(5));
+			g.drawRect(x, y, width, height);
+		}
+	}
+}
---
+++ src/entity/Food.java
@@ -0,0 +1,106 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+/**
+ * Food class represents Food
+ * @author Vachia Thoj
+ *
+ */
+public class Food extends Entity 
+{
+	//Color of Food
+	private Color color;
+	
+	//width and height Food starts out as
+	private int startWidth;
+	private int startHeight;
+	
+	//Speed that food grows
+	private int growthRate;
+	
+	//Growth timer and delay
+	private long growTimer;
+	private long growDelay;
+	
+	//Flag to see if food has finished growing
+	private boolean doneGrowing;
+	
+	//Default color of Food
+	private static final Color DEFAULT_COLOR = Color.BLUE;
+	
+	/**
+	 * Constructor
+	 * @param x integer x coordinate of Food
+	 * @param y integer y coordinate of Food
+	 * @param width integer width (pixels) of Food
+	 * @param height integer height (pixels) of Food
+	 */
+	public Food(int x, int y, int width, int height)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.growthRate = 4;
+		this.startWidth = width / 4;
+		this.startHeight = height / 4;
+		
+		this.growTimer = System.nanoTime();
+		this.growDelay = 50;
+		this.doneGrowing = false;
+		
+		this.color = DEFAULT_COLOR;
+	}
+	
+	//Getter Methods
+	public boolean isDoneGrowing() {return doneGrowing;}
+	public Color getColor() {return color;}
+	
+	//Setter Methods
+	public void setDoneGrowing(boolean b) {doneGrowing = b;}
+	public void setColor(Color color) {this.color = color;}
+	
+	/**
+	 * Method that updates Food
+	 */
+	public void update()
+	{
+		if(((System.nanoTime() - growTimer) / 1000000) > growDelay)
+		{
+			grow();
+		}
+	}
+	
+	/**
+	 * Method that makes Food "grow"
+	 */
+	private void grow()
+	{
+		//stop growing if target width and height are reached
+		if(startWidth == width || startHeight == height)
+		{
+			doneGrowing = true;
+			return;
+		}
+		
+		//increase width and height of Food
+		startWidth += growthRate;
+		startHeight += growthRate;
+		
+		growTimer = System.nanoTime();
+	}
+	
+	/**
+	 * Method that draws the Food
+	 * @param g The Graphics2D object to be drawn on
+	 */
+	public void draw(Graphics2D g)
+	{
+		g.setColor(color);
+		int newX = ((x + x + width) / 2) - (startWidth / 2);
+		int newY = ((y + y + height) / 2) - (startHeight / 2);
+		g.fillRect(newX, newY, startWidth, startHeight);
+	}
+}
